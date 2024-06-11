f26e9b34c1ae8a6c01d0a09f399a8ba36c4c90ba
karolina <dornelas.karolina@gmail.com>
2024-06-11 13:54:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ function compareTexts() {
         return;
     }
 
-    const words1 = text1.match(/\b\w+[\w\.,-]*\b/g) || [];
-    const words2 = text2.match(/\b\w+[\w\.,-]*\b/g) || [];
+    const words1 = text1.match(/(?<!\w)[\wà-úÀ-Ú]+(?!\w)/g) || [];
+    const words2 = text2.match(/(?<!\w)[\wá-ùÀ-Ù]+(?!\w)/g) || [];
 
     const commonWords = words1.filter(word => words2.includes(word));
 
@@ -16,9 +16,13 @@ function compareTexts() {
     const similarity = (commonWords.length / totalWords) * 100;
 
     function highlightWords(text, words) {
-        words.forEach(word => {
-            const regex = new RegExp(`(\\b${word.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}\\b)`, 'gi');
-            text = text.replace(regex, '<span class="highlight">$1</span>');
+        console.log(words)
+        const singularWords = [...new Set(words)];
+        console.log(singularWords)
+        singularWords.forEach(word => {
+            const specialCharacters = word.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+            const regex = new RegExp (`(?<![A-ZÀ-Ú])(${specialCharacters})(?![A-ZÀ-Ú])`, 'gi');
+            text = text.replace(regex, `<span class="highlight">${word}</span>`);
         });
         return text;
     }
@@ -29,6 +33,25 @@ function compareTexts() {
     document.getElementById('highlightedText1').innerHTML = highlightedText1;
     document.getElementById('highlightedText2').innerHTML = highlightedText2;
     document.getElementById('similarityPercentage').innerText = `Porcentagem de similaridade textual: ${similarity.toFixed(2)}%`;
+
+    document.getElementById('results').style.display = 'block';
+    document.getElementById('rebootFields').style.display = 'block';
+    document.getElementById('get-pdf').style.display = 'block';
+
+}
+
+//resetar os campos de texto
+function rebootFields() {
+    document.getElementById('text1').value = '';
+    document.getElementById('text2').value = '';
+
+    document.getElementById('highlightedText1').textContent = '';
+    document.getElementById('highlightedText2').textContent = '';
+    document.getElementById('similarityPercentage').textContent = '';
+
+    document.getElementById('results').style.display = 'none';
+    document.getElementById('rebootFields').style.display = 'none';
+    document.getElementById('get-pdf').style.display = 'none';
 }
 
 function getPdf() {
